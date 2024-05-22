6c21e11935019b4391187d70d99923a51b215bd6
anton <anton.kozintsov@gmail.com>
2024-05-22 17:15:16 +0200

    commit

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-import { useEffect, useState } from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import './env';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Ed25519Keypair } from "@mysten/sui.js/keypairs/ed25519";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { generateNonce, generateRandomness } from '@mysten/zklogin';
+import { SuiClient } from "@mysten/sui.js/client";
+import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from "expo-web-browser";
-import * as Google from "expo-auth-session/providers/google";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
 WebBrowser.maybeCompleteAuthSession();
 
-export default function App() {
-  const [token, setToken] = useState("");
-  const [userInfo, setUserInfo] = useState(null);
+export default function EphemeralKeyPair() {
 
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    androidClientId: "",
-    iosClientId: "",
-    webClientId: "",
-  });
+  // const redirectUri = AuthSession.makeRedirectUri();
+
+  const config = {
+    androidClientId: '162663894145-7s2u94c6s4k37345f66d6cbd1ndpsbm2.apps.googleusercontent.com',
+    webClientId: '162663894145-3lj29d0eqa71bhvs2slsau96612kmga7.apps.googleusercontent.com',
+  };
+
+  const [request, response, promptAsync] = Google.useIdTokenAuthRequest(config, nonce)
+  const [userInfo, setUserInfo] = useState(null)
+  const [token, setToken] = useState("");
+
+  const suiClient = new SuiClient({ url: "https://fullnode.devnet.sui.io" });
+
+  const randomness = generateRandomness();
+
+  const getUserInfo = async (token) => {
+    if (!token) return;
+    try {
+      const res = await fetch(
+          "https://www.googleapis.com/userinfo/v2/me",
+          {
+            headers: { Authorization: `Bearer ${token}` },
+          }
+      );
+
+      const user = await res.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error) {
+      // Add your own error handler here
+    }
+  };
+
+  const getLocalUser = async () => {
+    const data = await AsyncStorage.getItem("@user");
+    if (!data) return null;
+    return JSON.parse(data);
+  };
 
   useEffect(() => {
     handleEffect();
@@ -25,7 +59,7 @@ export default function App() {
     console.log("user", user);
     if (!user) {
       if (response?.type === "success") {
-        // setToken(response.authentication.accessToken);
+        setToken(response.authentication.accessToken);
         getUserInfo(response.authentication.accessToken);
       }
     } else {
@@ -34,80 +68,49 @@ export default function App() {
     }
   }
 
-  const getLocalUser = async () => {
-    const data = await AsyncStorage.getItem("@user");
-    if (!data) return null;
-    return JSON.parse(data);
-  };
+  const createRandomEphemeralKeyPair = async () => {
 
-  const getUserInfo = async (token) => {
-    if (!token) return;
-    try {
-      const response = await fetch(
-        "https://www.googleapis.com/userinfo/v2/me",
-        {
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
+    await promptAsync()
 
-      const user = await response.json();
-      await AsyncStorage.setItem("@user", JSON.stringify(user));
-      setUserInfo(user);
-    } catch (error) {
-      // Add your own error handler here
-    }
-  };
+    const user = getUserInfo(response)
+
+    const ephemeralKeyPair = new Ed25519Keypair();
+
+    console.log(ephemeralKeyPair.getPublicKey(), ephemeralKeyPair.getSecretKey())
+
+    const { epoch } = await suiClient.getLatestSuiSystemState()
+
+    const maxEpoch = Number(epoch) + 10;
+
+    const nonce = generateNonce(
+        ephemeralKeyPair.getPublicKey(),
+        maxEpoch,
+        randomness
+    );
+
+  }
 
   return (
-    <View style={styles.container}>
-      {!userInfo ? (
-        <Button
-          title="Sign in with Google"
-          disabled={!request}
-          onPress={() => {
-            promptAsync();
-          }}
-        />
-      ) : (
-        <View style={styles.card}>
-          {userInfo?.picture && (
-            <Image source={{ uri: userInfo?.picture }} style={styles.image} />
-          )}
-          <Text style={styles.text}>Email: {userInfo.email}</Text>
-          <Text style={styles.text}>
-            Verified: {userInfo.verified_email ? "yes" : "no"}
-          </Text>
-          <Text style={styles.text}>Name: {userInfo.name}</Text>
-          {/* <Text style={styles.text}>{JSON.stringify(userInfo, null, 2)}</Text> */}
-        </View>
-      )}
-      <Button
-        title="remove local store"
-        onPress={async () => await AsyncStorage.removeItem("@user")}
-      />
-    </View>
+      <View style={styles.container}>
+        <Text>Your key pair: {}</Text>
+        <Pressable style={styles.btn} onPress={async() => await createRandomEphemeralKeyPair()}>
+          <Text>Create random ephemeral KeyPair</Text>
+        </Pressable>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  text: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  card: {
+  btn: {
     borderWidth: 1,
-    borderRadius: 15,
-    padding: 15,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-  },
+    borderColor: "black",
+    padding: 10,
+    borderRadius: 10
+  }
 });
